35c73c54ba241220b76dbb3c7fd443fee6145b46
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2015-12-08 13:17:28 -0600

    There's not any documentation... yet

--- cerebro/package.js
+++ cerebro/package.js
@@ -7,7 +7,7 @@ Package.describe({
   git: 'https://github.com/NUDelta/ce-engines',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: null
 });
 
 Package.onUse(function(api) {
