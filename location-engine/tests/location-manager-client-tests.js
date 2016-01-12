83f50d760d7761e9f26e791ac8386e44f97f3aa6
aspin <tsunami70875@gmail.com>
2016-01-12 01:05:59 -0600

    added strings to test cases

--- location-engine/tests/location-manager-client-tests.js
+++ location-engine/tests/location-manager-client-tests.js
@@ -17,14 +17,14 @@ let stubTracker = new StubTracker(),
     timesCalled = 0;
 
 Tinytest.addAsync('trackUpdates sets current according to the first argument', (test, next) => {
-  test.equal(LocationManager._current, {});
+  test.equal(LocationManager._current, {}, 'instantiation');
   LocationManager.trackUpdates(stubTracker,
    function(location) {
      return 1;
    },
    function(transform, location) {
-     test.equal(transform, 1);
-     test.equal(LocationManager._current.struct, 1);
+     test.equal(transform, 1, 'transformed value should be passed through');
+     test.equal(LocationManager._current.struct, 1, 'transformed value should be stored');
      timesCalled += 1;
 
      if (timesCalled == 1) {
@@ -36,7 +36,6 @@ Tinytest.addAsync('trackUpdates sets current according to the first argument', (
 
 Tinytest.add('trackUpdates should recall the second argument on reruns', (test) => {
   let currentTimesCalled = timesCalled;
-  stubTracker.rerun();
-  console.log(timesCalled);
+  stubTracker.rerun(); // provide new stub? old tests called
   test.equal(currentTimesCalled + 1, timesCalled);
 });
