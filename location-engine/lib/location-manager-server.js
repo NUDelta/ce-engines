6c241b67a6e800559e302949dbcd9dfb26272b28
aspin <tsunami70875@gmail.com>
2016-02-03 12:47:59 -0600

    added methods and tests to compute lat/lng bounding boxes

--- location-engine/lib/location-manager-server.js
+++ location-engine/lib/location-manager-server.js
@@ -1,3 +1,5 @@
+const earthRadius = 6371000;
+
 LocationManagerServer = class LocationManagerServer {
   constructor() {
 
@@ -14,7 +16,7 @@ LocationManagerServer = class LocationManagerServer {
   computeDistance(location1, location2) {
     // https://en.wikipedia.org/wiki/Haversine_formula
     // TODO: check this for accuracy + understand this
-    const earthRadius = 6371000;
+    // maximum error should be 0.5%
     let dLat = this._degreeToRadian(location1.lat - location2.lat),
         dLng = this._degreeToRadian(location1.lng - location2.lng);
 
@@ -29,19 +31,47 @@ LocationManagerServer = class LocationManagerServer {
   _degreeToRadian(degree) {
     return degree * Math.PI / 180;
   }
+
+  computeBoundsAround(location, radius) {
+  }
+
+  _computeLatBounds(latitude, distance) {
+    // TODO: deal with poles
+    /*
+     using:
+       circumference = 2 * pi * earthR
+       circ * x / 360 = distance, solve for x
+    */
+    let degreeDiff = 360 / (2 * Math.PI * earthRadius / distance),
+        bounds = [];
+    bounds.push(this._incLatitude(latitude, -degreeDiff));
+    bounds.push(this._incLatitude(latitude, degreeDiff));
+    return bounds;
+  }
+
+  _incLatitude(latitude, difference) {
+    return Math.max(Math.min(latitude + difference, 90), -90);
+  }
+
+  _computeLngBounds(location, distance) {
+    /*
+      similar algorithm as _complteLatBounds, but adjusts the radius
+      to account for various size circles on differents lines of latitude
+    */
+    let latCircleRadius = earthRadius * Math.cos(location.lat);
+
+    let degreeDiff = 360 / (2 * Math.PI * latCircleRadius / distance),
+        bounds = [];
+    bounds.push(this._incLatitude(location.lng, -degreeDiff));
+    bounds.push(this._incLatitude(location.lng, degreeDiff));
+    return bounds;
+  }
+
+  _incLongitude(longitude, difference) {
+    let partial = (longitude + 180 + difference) % 360 - 180;
+    if (partial < -180) {
+      partial = 180 - Math.abs(180 + partial);
+    }
+    return partial;
+  }
 };
-
-/*
- public static float distFrom(float lat1, float lng1, float lat2, float lng2) {
- double earthRadius = 6371000; //meters
- double dLat = Math.toRadians(lat2-lat1);
- double dLng = Math.toRadians(lng2-lng1);
- double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
- Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
- Math.sin(dLng/2) * Math.sin(dLng/2);
- double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
- float dist = (float) (earthRadius * c);
-
- return dist;
- }
- */
--- location-engine/tests/location-manager-server-tests.js
+++ location-engine/tests/location-manager-server-tests.js
@@ -1,6 +1,14 @@
+function isWithinRange(diff, range) {
+  return Math.abs(diff) < range;
+}
+
+Tinytest.add('converts degrees to radians', (test) => {
+  let conversion = LocationManager._degreeToRadian(180);
+  test.isTrue(isWithinRange(conversion - Math.PI, 0.05));
+});
 
 Tinytest.add('calculates distance between two geocoordinates properly', (test) => {
-  // Based on http://www.nhc.noaa.gov/gccalc.shtml
+  // truth: http://www.csgnetwork.com/gpsdistcalc.html
   let location1 = {
     lat: 47.556,
     lng: 58.122,
@@ -8,8 +16,72 @@ Tinytest.add('calculates distance between two geocoordinates properly', (test) =
     lat: 48.031,
     lng: 58.203
   };
-  let actualDistance = 53000,
+  let actualDistance = 53128,
       computedDistance = LocationManager.computeDistance(location1, location2);
 
-  test.isTrue(Math.abs(computedDistance - actualDistance) < 200, 'distance computed is inaccurate/incorrect');
+  test.isTrue(isWithinRange(computedDistance - actualDistance, 50), `distance too great: expected ${actualDistance}, got ${computedDistance}`);
+});
+
+Tinytest.add('computes bounding box around a location', (test) => {
+  let techInstitute = {
+    lat: 42.0564563,
+    lng: -87.7494751
+  }, radius = 1000;
+});
+
+Tinytest.add('latitude bounds are computed properly', (test) => {
+  let bounds = LocationManager._computeLatBounds(40, 50);
+  test.isTrue(bounds[1] > bounds[0], 'bounds are out of order');
+  let example = {
+    lat: 40,
+    lng: 60
+  }, leftShifted = {
+    lat: bounds[0],
+    lng: 60
+  }, rightShifted = {
+    lat: bounds[1],
+    lng: 60
+  };
+  let distanceLeft = LocationManager.computeDistance(example, leftShifted),
+      distanceRight = LocationManager.computeDistance(example, rightShifted);
+
+  test.isTrue(isWithinRange(distanceLeft - 50, 10), `bounds are wrong left-shifted distance: expected: 50, got ${distanceLeft}`);
+  test.isTrue(isWithinRange(distanceRight - 50, 10), `bounds are wrong right-shifted distance: expected: 50, got ${distanceLeft}`);
+});
+
+Tinytest.add('latitude changes are wrapped properly', (test) => {
+  test.equal(LocationManager._incLatitude(40, 10), 50, 'normal change fails');
+  test.equal(LocationManager._incLatitude(80, 20), 90, 'crossing north pole change fails');
+  test.equal(LocationManager._incLatitude(90, 20), 90, 'starting from north pole fails');
+  test.equal(LocationManager._incLatitude(10, -20), -10, 'crossing equator fails');
+  test.equal(LocationManager._incLatitude(-80, -20), -90, 'crossing south pole fails');
+  test.equal(LocationManager._incLatitude(-90, 20), -70, 'starting from south pole fails');
+});
+
+Tinytest.add('longitude bounds are computed properly', (test) => {
+  let example = {
+    lat: 40,
+    lng: 60
+  }, bounds = LocationManager._computeLngBounds(example, 50);
+  let leftShifted = {
+    lat: 40,
+    lng: bounds[0]
+  }, rightShifted = {
+    lat: 40,
+    lng: bounds[1]
+  };
+  let distanceLeft = LocationManager.computeDistance(example, leftShifted),
+      distanceRight = LocationManager.computeDistance(example, rightShifted);
+
+  test.isTrue(isWithinRange(distanceLeft - 50, 10), `bounds are wrong left-shifted distance: expected: 50, got ${distanceLeft}`);
+  test.isTrue(isWithinRange(distanceRight - 50, 10), `bounds are wrong right-shifted distance: expected: 50, got ${distanceLeft}`);
+});
+
+Tinytest.add('longitude changes are wrapped properly', (test) => {
+  test.equal(LocationManager._incLongitude(40, 10), 50, 'normal change fails');
+  test.equal(LocationManager._incLongitude(170, 20), -170, 'crossing date line east to west change fails');
+  test.equal(LocationManager._incLongitude(180, 20), -160, 'starting from date line east fails');
+  test.equal(LocationManager._incLongitude(10, -20), -10, 'crossing Greenwich fails');
+  test.equal(LocationManager._incLongitude(-170, -20), 170, 'crossing date line west to east fails');
+  test.equal(LocationManager._incLongitude(-180, -20), 160, 'starting from date line west fails');
 });
