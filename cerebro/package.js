692479688d07661b5c824c733d8aa202b84d9ef1
aspin <tsunami70875@gmail.com>
2016-02-28 11:31:16 -0600

    Fixed dependency

--- cerebro/package.js
+++ cerebro/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'collectiveexperiences:cerebro',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Facilitates querying of users based on user.profile fields',
   git: 'https://github.com/NUDelta/ce-engines',
   documentation: null
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use(['ecmascript', 'accounts-base', 'accounts-password', 'accounts-oauth', 'oauth1',
-    'erasaur:meteor-lodash@0.1.0', 'aspin:location-engine@0.0.6', 'percolate:synced-cron@1.3.0']);
+    'erasaur:meteor-lodash@0.1.0', 'collectiveexperiences:location-engine@0.0.1', 'percolate:synced-cron@1.3.0']);
   api.export('CerebroServer');
   api.export('CerebroClient');
   api.export('Cerebro');
